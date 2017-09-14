fc3ecdbb21ebc9080e04aa06270ced16e937aeff
Safaet Hossain <tuhin_42@hotmail.com>
2017-09-15 00:54:40 +0600

    first commit

---
+++ Home_page.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Prijkandi High School,Mirzapur,Raipura,Narsigndi</title>
+		<meta charset="utf-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  	<link rel="stylesheet" href="assets/css/style.css">
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  	<link rel="stylesheet" type="text/css" href="css/Home_style.css">
+	</head>
+	<body>
+		<div id = "header">
+			<div id="imgs">
+			<span>
+				<img id = "logo" src = "img/logo.png">
+			</span>
+			<span style = "font-size: 26px; color: gray;">
+				<b>Prijkandi High School</b>
+				<b style = "font-size: 18px;">"Mirzapur,Raipura,Narsigndi"</b>
+			</span>
+			</div>
+		</div>
+		<div id = "menubar">
+			<!--<nav class = "navbar navbar-default" id = "topnavbar">
+				<div class = "container-fluid">
+					
+					<div class = "navbar-header" style = "padding-left: 108px;">
+						<a class = "navbar-brand" href = "#"><b><i>PHS</i></b></a>
+					</div>
+					-->
+					<div class = "navbar-collapse collapse">
+						<ul class = "nav navbar-nav">
+							<li><a href="#">Home</a></li>
+							<li><a href = "#">Result</a></li>
+							<li><a href = "#">Management</a></li>
+							<li><a href="#">Academic</a></li>
+							<li><a href = "#">About</a></li>
+							<li><a href = "#">Contact</a></li>
+							<li><a href = "#">Admission</a></li>
+							<li><a href="#">Photo</a></li>
+						</ul>
+					</div>
+			
+		</div>
+	</body>
+</html>
