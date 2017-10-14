bf8e022a7e43c4bacc656079cb9a10b36412c021
Safaet Hossain <tuhin_42@hotmail.com>
2017-10-15 01:16:25 +0600

    result_add file updated

--- Result_add.php
+++ Result_add.php
@@ -1,23 +1,90 @@
 	<?php 
 		include("shared/User_Header.php");
 	?>
-		<div class="container" style="padding-top: 20px;">
+		<div class="container">
 			<div class="panel panel-primary">
 				<div class="panel-heading">
 					Result Add
 				</div>
 				<div class="panel-body">
 					<ul class="nav nav-tabs">
-						<li class="active">
-							<a data-toggle="tab" href="#class">Class</a>
-						</li>
+						<li class="active"><a data-toggle="tab" href="#class">Class</a></li>
 						<li><a data-toggle="tab" href="#ssc">Ssc</a></li>
 						<li><a data-toggle="tab" href="#jsc">Jsc</a></li>
 					</ul>
 					<div class="tab-content">
-						<div id="class" class="tab-pane fade in active" style="padding-top: 20px;">sdfsdfa</div>
-						<div id="ssc" class="tab-pade fade" style="padding-top: 20px;">sdfsdf</div>
-						<div id="jsc" class="tab-pade fade" style="padding-top: 20px;">sdfsd</div>
+						<div id="class" class="tab-pane fade in active">
+							<div class="panel-group" id="accordion">
+								<div class="panel panel-success">
+									<div class="panel-heading">
+										<a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Add</a>
+									</div>
+									<div id="collapse1" class="panel-collapse collapse in">
+										<div class="panel-body">
+											Add result
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										<a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Show</a>
+									</div>
+									<div id="collapse2" class="panel-collapse collapse in">
+										<div class="panel-body">
+											Show result
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div id="ssc" class="tab-pade fade">
+							<div class="panel-group" id="accordion2">
+								<div class="panel panel-success">
+									<div class="panel-heading">
+										<a data-toggle="collapse" data-parent="#accordion2" href="#collapse3">Add</a>
+									</div>
+									<div id="collapse3" class="panel-collapse collapse in">
+										<div class="panel-body">
+											Add result
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										<a data-toggle="collapse" data-parent="#accordion2" href="#collapse4">Show</a>
+									</div>
+									<div id="collapse4" class="panel-collapse collapse in">
+										<div class="panel-body">
+											Show result
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div id="jsc" class="tab-pade fade">
+							<div class="panel-group" id="accordion3">
+								<div class="panel panel-success">
+									<div class="panel-heading">
+										<a data-toggle="collapse" data-parent="#accordion3" href="#collapse5">Add</a>
+									</div>
+									<div id="collapse5" class="panel-collapse collapse in">
+										<div class="panel-body">
+											Add result
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										<a data-toggle="collapse" data-parent="#accordion3" href="#collapse6">Show</a>
+									</div>
+									<div id="collapse6" class="panel-collapse collapse in">
+										<div class="panel-body">
+											Show result
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>		
